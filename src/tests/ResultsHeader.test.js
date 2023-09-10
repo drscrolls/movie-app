e5f8a1329cdad8f2ea27d7f03594a92e1473bd27
Andrew <dr.scrolls@gmail.com>
2023-09-10 07:42:50 +0000

    minor text change on test description

--- src/tests/ResultsHeader.test.js
+++ src/tests/ResultsHeader.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import ResultsHeader from '../components/ResultsHeader';
 
-test('renders title on header', () => {
+test('renders titles on header', () => {
   render(<ResultsHeader />);
   const posterElement = screen.getByText(/Poster/i);
   const titleElement = screen.getByText(/Title/i);
